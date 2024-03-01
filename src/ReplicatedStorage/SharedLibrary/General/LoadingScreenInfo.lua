411de1f176937e7ea9df845199770942d61845b6
Adam <adam.worden06@gmail.com>
2024-03-01 09:10:10 +0000

    Tween in Games Page

--- src/ReplicatedStorage/SharedLibrary/General/LoadingScreenInfo.lua
+++ src/ReplicatedStorage/SharedLibrary/General/LoadingScreenInfo.lua
@@ -4,6 +4,9 @@ return {
     },
     TweenData = {
         LoadingTween = TweenInfo.new(math.random(100, 120)/10, Enum.EasingStyle.Linear, Enum.EasingDirection.In),
-        ClearScreenTween = TweenInfo.new(3, Enum.EasingStyle.Bounce, Enum.EasingDirection.Out)
+        LoadingHighlight = TweenInfo.new(1.25, Enum.EasingStyle.Sine, Enum.EasingDirection.In),
+        ClearScreenTween = TweenInfo.new(1, Enum.EasingStyle.Bounce, Enum.EasingDirection.Out),
+        WholeScreenClear = TweenInfo.new(2, Enum.EasingStyle.Bounce, Enum.EasingDirection.Out),
+        LoadGamesPage = TweenInfo.new(1.5, Enum.EasingStyle.Quart, Enum.EasingDirection.In)
     }
 }
--- src/StarterPlayer/StarterPlayerScripts/ClientLibrary/Interface/LoadingCore.lua
+++ src/StarterPlayer/StarterPlayerScripts/ClientLibrary/Interface/LoadingCore.lua
@@ -17,6 +17,8 @@ local MainMenu = Player.PlayerGui:WaitForChild("MainMenu")
 local LoadingScreen = MainMenu:WaitForChild("LoadingScreen")
 local LoadingBar = LoadingScreen:WaitForChild("LoadingBar")
 
+local GamesPage = MainMenu:WaitForChild("Games")
+
 local Library = {
     LoadingScreenInfo = require(SharedLibrary:WaitForChild("General"):WaitForChild("LoadingScreenInfo"))
 }
@@ -24,9 +26,8 @@ local Library = {
 --// Functions
 
 function LoadBar()
+    LoadingScreen.Visible, GamesPage.Visible = true, false;
 
-    local IntValue = Instance.new("IntValue")
-    IntValue.Parent = script
 
     local Tween = TweenService:Create(LoadingBar:WaitForChild("Subbar"), Library.LoadingScreenInfo.TweenData.LoadingTween, {Size = UDim2.new(1, 0, 1, 0)} )
     Tween:Play()
@@ -39,6 +40,29 @@ function LoadBar()
     PercentageDetection:Disconnect()
     if Tween then Tween:Destroy() end
 
+    --[[     This was a really bad idea.     ]]--
+    -- LoadingBar.Subbar:WaitForChild("Frame").Transparency = 0
+    -- for i=1, 3 do
+    --     task.wait(0.1)
+    --     LoadingBar.Subbar.Frame.Position = UDim2.new(0, 0, 0, 0)
+    --     Tween = TweenService:Create(LoadingBar.Subbar.Frame, Library.LoadingScreenInfo.TweenData.LoadingHighlight, {Position = UDim2.new(1, 0, 0, 0)})
+    --     Tween:Play()
+    --     Tween.Completed:Wait()
+    --     if Tween then Tween:Destroy() end
+    -- end
+
+    task.wait(0.2)
+
+    TweenService:Create(LoadingBar, Library.LoadingScreenInfo.TweenData.ClearScreenTween, {Position = UDim2.new(0.5, 0, 1.1, 0)}):Play()
+    TweenService:Create(LoadingScreen.PercentageText, Library.LoadingScreenInfo.TweenData.ClearScreenTween, {Position = UDim2.new(0.5, 0, 1.1, 0)}):Play()
+    TweenService:Create(LoadingScreen.GameTips, Library.LoadingScreenInfo.TweenData.ClearScreenTween, {Position = UDim2.new(0.5, 0, 1.1, 0)}):Play()
+
+    task.wait(Library.LoadingScreenInfo.TweenData.ClearScreenTween.Time+0.1)
+    TweenService:Create(LoadingScreen, Library.LoadingScreenInfo.TweenData.WholeScreenClear, {Position = UDim2.new(0.5, 0, 1.5, 0)}):Play()
+
+    GamesPage.Visible = true
+    GamesPage.Position = UDim2.new(0.5, 0, -1.5, 0)
+    TweenService:Create(GamesPage, Library.LoadingScreenInfo.TweenData.LoadGamesPage, {Position = UDim2.new(0.5, 0, 0.5, 0)}):Play()
 end
 
 --// Module Functions
@@ -51,6 +75,9 @@ function Core:Initialise()
         until game:IsLoaded()
 
         LoadBar()
+
+
+
     end)
 end
 
